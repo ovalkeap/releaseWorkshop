fd909d031a28479fbe6e013feefc3f2fdc49bf15
ovalkeap <onni.valkeapaa@kauppalehti.fi>
2012-09-27 09:55:20 +0300

    korjaus

--- src/main/java/fi/kauppalehti/domain/Account.java
+++ src/main/java/fi/kauppalehti/domain/Account.java
@@ -6,10 +6,11 @@ import java.util.concurrent.atomic.AtomicLong;
 public class Account {
 
 	private final Long id;	
-	private final long test;
+	private final Long test;
 	
 	public Account() {
 		this.id = idSequence.getAndIncrement();
+		this.test = null;
 	}
 	
 	public Long getId() {
